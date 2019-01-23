4ed5d2bb1b64dab39316f51d19483c44dc562ee1
jeremythunell <jeremythunell@gmail.com>
2019-01-23 14:00:09 -0800

    Added "generate <currency> [number]" command to CLI

--- cli.js
+++ cli.js
@@ -2,5 +2,42 @@
 
 // grab command line arguments
 const [,, ...args] = process.argv;
+const cw = require('./cryptowallets');
 
-console.log(`${args}`);
+function generateNAddresses(currency, n){
+	var wallets = [];
+	for(var i = 0; i < n; i++){
+		wallets.push(cw.generateWallet(currency));
+	}
+	return wallets;
+}
+
+async function generateNMoneroAddresses(n){
+	var wallets = [];
+	for(var i = 0; i < n; i++){
+		var wallet = await cw.generateWallet("XMR")
+		wallets.push(wallet);
+	}
+	return wallets;
+}
+
+switch(args[0]){
+	case "generate":
+	
+		if(args.length < 2){
+			console.log("Usage: generate <currency> [number]");
+			
+			break;
+		}
+		if(args[1].toLowerCase() == "xmr"){
+			generateNMoneroAddresses((args.length > 2)? args[2] : 1).then(function(wallets){
+				console.log(wallets);
+			});
+			
+			break;
+		}
+		console.log(generateNAddresses(args[1], (args.length > 2)? args[2] : 1));
+		break;
+	default:
+		console.log("unknown command " + args[0]);
+}
