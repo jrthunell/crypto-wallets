ee6fb699974ebf315cf2d653fd45d41f07826886
jeremythunell <jeremythunell@gmail.com>
2019-01-24 17:16:12 -0800

    Added help and verify commands to the CLI

--- cli.js
+++ cli.js
@@ -27,8 +27,31 @@ async function generateNMoneroAddresses(n){
 }
 
 module.exports = {
+	helpMsg: `
+Usage:
+crypto-wallet <command> [arguments]
+	
+Commands:
+help, h, -help, --help: Prints this help message
+	
+generate <currency> [number]: 
+    Generates and prints private keys and addresses for the specified currency.
+    If [number] is provided, that many wallets will be generated.
+    If no [number] is provided, one wallet will be generated.
+    Supported currencies:
+    BTC, BCH, ETH, DOGE, LTC, NMC, PPC, XMR
+		
+verify <currency> <privateKey> <address>: 
+    Verifies that the given privateKey corresponds to the given address in the given currency.`,
 	parseArgs(args){
 		switch(args[0]){
+			case "help":
+			case "h":
+			case "-help":
+			case "--help":
+			case undefined:
+				console.log(this.helpMsg);
+				break;
 			case "generate":
 				if(args.length < 2){
 					console.log("Usage: generate <currency> [number]");
@@ -43,6 +66,29 @@ module.exports = {
 				}
 				console.log(generateNAddresses(args[1], (args.length > 2)? args[2] : 1));
 				break;
+			case "verify":
+				if(args.length < 4){
+					console.log("Usage: verify <currency> <privateKey> <address>");
+					break;
+				} else {
+					try{
+						var res = cw.verifyPrivateKey(args[1], args[2], args[3]);
+						if(res === true)
+							console.log("Success: The private key matches the address");
+						else if(res.then){ // if a promise is returned, resolve it
+							res.then(function(res){
+								if(res)
+									console.log("Success: The private key matches the address");
+								else
+									console.log("Failure: The private key does not match the address");
+							});
+						} else
+							console.log("Failure: The private key does not match the address");
+					}catch(err){
+						console.log("Failure: The private key does not match the address");
+					}
+					break;
+				}
 			default:
 				console.log("unknown command " + args[0]);
 		}
